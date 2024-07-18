bf7fe0bd97bf0530f13dc545ce22f5465d82d6e3
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-07-18 14:18:47 -0300

    Create centimetros.py

---
+++ centimetros.py
@@ -0,0 +1,12 @@
+# 7. Crie uma variável e nela armazene um valor em metros e converta este valor para centimetros
+
+# metro = 1.81
+# centimetros = metro * 100
+# print(centimetros)
+
+metros = 256
+coeficienteDEConversao = 100
+
+centimetros = metros * coeficienteDEConversao
+
+print(f'O valor em centímetros é: {centimetros:.2f}')
